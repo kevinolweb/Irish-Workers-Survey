b7952c04b8ac022a77e4fdf3b2fa820b23452642
kevinolweb <kevin.oleary91@gmail.com>
2022-12-02 18:27:45 +0000

    Main function started

--- run.py
+++ run.py
@@ -16,4 +16,13 @@ last_years_data = SHEET.worksheet('2021')
 
 data = last_years_data.get_all_values()
 
-print(data)
+def get_survey_data():
+    """
+    This function gets survey inputs from user
+    """
+    print("Thank you for your interest in taking part in this survey.\n It is a survey of full time workers in Ireland. \n We ask that you answer all questions truthfully.")
+
+    input_data=input("Please enter your salary e.g 5000 \n")
+    print(f"Your salary is €{input_data}")
+
+get_survey_data()
